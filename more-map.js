248d79d67fbc22eb43410000ea7de08f5d2877c8
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-01 16:12:12 +0600

    more map

--- more-map.js
+++ more-map.js
@@ -1,4 +1,9 @@
 const numbers = [30, 20, 41, 3, 6, 3, 21, 23, 12];
 const doubled = numbers.map(num => num * 2);
 
-console.log(doubled)
+// console.log(doubled)
+
+const friends =['Tom','john','Micheal','Oliver'];
+
+const lengths =friends.map(frnd => frnd.length);
+console.log(lengths)
