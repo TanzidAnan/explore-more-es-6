8d0693c2b32568b4aa9e8ab998aa96dd816470f7
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-01 16:45:16 +0600

    filter

---
+++ filter.js
@@ -0,0 +1,7 @@
+// filter selects elements based on a condition and returns an array with the elements that fulfilled the 
+
+const numbers = [1, 3, 63, 52, 45, 74, 14, 65];
+
+const filters =numbers.filter(p =>p>50);
+console.log(filters)
+
