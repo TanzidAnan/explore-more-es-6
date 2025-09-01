9fcba724907b1ef671ae52b8d96ddae3103a2e5a
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-01 16:52:10 +0600

    find

---
+++ find.js
@@ -0,0 +1,12 @@
+const numbers = [1, 3, 63, 52, 45, 74, 14, 65];
+
+const selected =numbers.find(num => num >60)
+
+console.log(selected);
+
+/**
+ * 1.map
+ * 2.forEach
+ * 3. filter
+ * 4.find
+ * */ 
