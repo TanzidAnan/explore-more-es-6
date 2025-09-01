681662056956c298f3249c878a9022d25bd007c9
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-01 16:48:53 +0600

    filter

--- filter.js
+++ filter.js
@@ -2,6 +2,15 @@
 
 const numbers = [1, 3, 63, 52, 45, 74, 14, 65];
 
-const filters =numbers.filter(p =>p>50);
-console.log(filters)
+// const filters =numbers.filter(p =>p>50);
+
+const selected = numbers.filter(p => p % 2 === 0);
+// console.log(selected)
+
+// console.log(filters)
+
+const friends =['Tom','john','Micheal','Oliver'];
+
+const OddFriends =friends.filter(friend => friend.length >4)
+console.log(OddFriends)
 
