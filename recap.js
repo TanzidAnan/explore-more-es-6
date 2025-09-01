776295de87eb917927c87e258cacc354ecc1efb9
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-01 14:02:44 +0600

    var let const

--- recap.js
+++ recap.js
@@ -1,1 +1,28 @@
-console.log(454)
+/**
+ * 1. var let const
+ * 2.default parameter
+ * 3. template string 
+ * 4.arrow function
+ * 5. destructuring and spread operator
+ * 6. keys values Object.entries
+ * 7.for Of used in array and string ,
+ * 8.for in loop used in object
+ * */ 
+
+const a =56;
+const numbers =[45,65,14];
+const person ={
+    name:'sakib khan'
+}
+
+const message =`Hi ${person.name} has a:${a} access to ${numbers}`;
+console.log(message)
+
+const square = x => x*x
+
+const sum =(a,b) => a+b
+
+
+const {age ,z, ...others} ={x:2, y:20, z:60, name:'Tanzid' ,age:20};
+
+const [fast,sec] =[]
