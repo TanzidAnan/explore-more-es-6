5f1e8dfedaec584c5f2806721d77cad7a0f31fde
TanzidAnan <tanzid.anan125@gmail.com>
2025-09-01 21:31:33 +0600

    class

--- class.js
+++ class.js
@@ -1,17 +1,36 @@
-const products =[
-    {id:1, name:'lenovo', price:65000},
-    {id:2, name:'dell', price:45000},
-    {id:3, name:'hp', price:110000},
-    {id:4, name:'mec', price:40000},
+const products = [
+    { id: 1, name: 'lenovo', price: 65000 },
+    { id: 2, name: 'dell', price: 45000 },
+    { id: 3, name: 'hp', price: 110000 },
+    { id: 4, name: 'mec', price: 40000 },
 ];
 
 class Product {
-    country ='bangladesh'
-    speak(talk){
+    country = 'bangladesh';
+    constructor(name) {
+        this.name = name;
+    }
+    speak(talk) {
         console.log(`talking about ${talk}`)
     }
 }
 
-const lenovo =new Product();
-console.log(lenovo);
+const lenovo = new Product('le le lenovo');
+// console.log(lenovo);
 lenovo.speak('oba kita kau')
+
+
+class Teacher {
+    constructor(name, subject) {
+        this.name = name;
+        this.subject = subject
+    }
+    lecture() {
+        console.log('Bangladesh')
+    }
+};
+
+const tapan =new Teacher('Tapon sir','Bangla');
+const rashid =new Teacher('Rashid sir','ICT');
+console.log(tapan)
+console.log(rashid)
